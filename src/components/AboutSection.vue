bfbc19344027ba3b1a6ad3ea25ad8101859f6d4e
Rodrigo <rodrigotteixeira7@gmail.com>
2024-07-19 00:15:53 +0000

    initial setup

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="about-section">"
+        <h2 class="title">about</h2>
+        <div class="about-content">
+            <p>enthusiastic about crafting <span class="highlight">user-friendly experiences</span> and tackling challenges with a collaborative spirit.</p>
+            <p>ready to showcase <span class="highlight">my skills</span> through personal projects and open-source <span class="highlight">contributions</span>  .</p>
+            <p>check out my portfolio and <span class="highlight">connect with me</span> to explore the possibilities!</p>
+        </div>
+    </section>
+</template>
+
+<script>    
+    export default {
+        name: 'AboutSection',
+        methods: {
+            moveToContact() {
+                // Logic for connecting goes here
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    section.about-section {}
+
+    h2#title {}
+
+    div.about-content {}
+
+    p {}
+
+    span.highlight {}
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="landing-page">
+        <h1 class="main-title">HI, I'M RODRIGO</h1>
+        <button @click="moveToContact()" id="connect-button">Connect with me!</button>
+    </section>
+</template>
+
+<script>    
+    export default {
+        name: 'LandingPage',
+        methods: {
+            moveToContact() {
+                // Logic for connecting goes here
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    section.landing-page {}
+    
+    h1#main-title {}
+
+    button#connect-button {}
+</style>
